aa9ee85b663c3042bfb2a58f112ed1192c4ba458
Mauro Moreno <moreno.mauro.emanuel@gmail.com>
2016-08-03 09:57:40 -0300

    Config indentation fix

--- resources/config.php
+++ resources/config.php
@@ -7,19 +7,17 @@ return [
 	| Switch this package on/off. Usefull for testing...
 	|--------------------------------------------------------------------------
 	*/
-
 	'enabled' => true,
 
-    /*
+	/*
     |--------------------------------------------------------------------------
     | File path where themes will be located.
     | Can be outside default views path EG: resources/themes
-    | Leave it null if you place your themes in the default views folder 
+    | Leave it null if you place your themes in the default views folder
     | (as defined in config\views.php)
     |--------------------------------------------------------------------------
     */
-
-    'themes_path' => null, // eg: realpath(base_path('resources/themes'))
+	'themes_path' => null, // eg: realpath(base_path('resources/themes'))
 
 	/*
 	|--------------------------------------------------------------------------
@@ -27,7 +25,6 @@ return [
 	| Available options: THROW_EXCEPTION | LOG_ERROR | ASSUME_EXISTS | IGNORE
 	|--------------------------------------------------------------------------
 	*/
-
 	'asset_not_found' => 'LOG_ERROR',
 
 	/*
@@ -36,7 +33,6 @@ return [
 	|  Themes::set('theme-name');
 	|--------------------------------------------------------------------------
 	*/
-
 	'active' => 'default',
 
 	/*
@@ -49,12 +45,11 @@ return [
 	| 		'asset-path' 	=> 'path-to-assets',   // defaults to: public/theme-name
 	|
 	|		// you can add your own custom keys and retrieve them with Theme::config('key');
-	| 		'key' 			=> 'value', 
+	| 		'key' 			=> 'value',
 	| 	],
 	|
 	|--------------------------------------------------------------------------
 	*/
-
 	'themes' => [
 
 		'default' => [
@@ -67,23 +62,23 @@ return [
 
 		/*--------------[ Example Structre ]-------------
 
-			// Recomended (all defaults) : Assets -> \public\BasicTheme , Views -> \resources\views\BasicTheme
+		// Recomended (all defaults) : Assets -> \public\BasicTheme , Views -> \resources\views\BasicTheme
 
-			'BasicTheme',
-
-
-			// This theme shares the views with BasicTheme but defines its own assets in \public\SomeTheme
-
-			'SomeTheme' => [
-				'views-path'	=> 'BasicTheme',
-			],
+		'BasicTheme',
 
 
-			// This theme extends BasicTheme and ovverides SOME views\assets in its folders
+		// This theme shares the views with BasicTheme but defines its own assets in \public\SomeTheme
 
-			'AnotherTheme' => [
-				'extends'	=> 'BasicTheme',
-			],
+		'SomeTheme' => [
+			'views-path'	=> 'BasicTheme',
+		],
+
+
+		// This theme extends BasicTheme and ovverides SOME views\assets in its folders
+
+		'AnotherTheme' => [
+			'extends'	=> 'BasicTheme',
+		],
 
 		------------------------------------------------*/
 	],
